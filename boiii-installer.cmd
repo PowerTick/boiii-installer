54f045fd7f2be33820095c40e9fa937f0a592d0e
PowerTick <82538976+PowerTick@users.noreply.github.com>
2024-02-19 23:08:12 +0200

    Cleaned up interface the interface, new feature!
    - Added aria2 arguments to improve the readability of the CLI
    - Intreagtion of the bypass script needed to run the client

--- boiii-installer.cmd
+++ boiii-installer.cmd
@@ -9,26 +9,26 @@ echo ! ============ Downloading launcher !
 if  exist .\t7_full_game\boiii.exe (
 	echo ! == Launcher is already downloaded !
 )
+
 if not exist .\t7_full_game\ (mkdir t7_full_game)
 if not exist .\t7_full_game\boiii.exe (
-	aria2c.exe --dir=.\t7_full_game https://web.archive.org/web/20230629141934/https://cdn.discordapp.com/attachments/700527387376353330/1121115413925871716/boiii.exe
-	echo The launcher has been DOWNLOADED!
+	aria2c --download-result=hide  --summary-interval=0 --console-log-level=warn --log-level=warn --console-log-level=error --dir=.\t7_full_game "https://web.archive.org/web/20230629141934/https://cdn.discordapp.com/attachments/700527387376353330/1121115413925871716/boiii.exe"
+	echo ! == The launcher has been downloaded !
 )
-
+echo.
 
 echo ! ==== Downloading client resources !
 
 if exist .\boiii.zip (
 	echo ! Client resources already downloaded
 )
-
 if not exist .\boiii.zip (
-	aria2c.exe https://web.archive.org/web/20230629142006/https://reactiongaming.us/community/attachments/boiii-zip.1130/
+	aria2c --download-result=hide --summary-interval=0 --console-log-level=warn --log-level=warn --console-log-level=error "https://web.archive.org/web/20230629142006/https://reactiongaming.us/community/attachments/boiii-zip.1130/"
 )
-
+echo.
 
 echo ! ===== Installing client resources !
-
+echo.
 if not exist "%HOMEDRIVE%%HOMEPATH%\AppData\Local\boiii\" (
 	echo Expand-Archive -Force "%cd%\boiii.zip" "%HOMEDRIVE%%HOMEPATH%\AppData\Local\boiii\" >%temp%\extracttemp.ps1
 	powershell Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser
@@ -37,15 +37,35 @@ if not exist "%HOMEDRIVE%%HOMEPATH%\AppData\Local\boiii\" (
 	echo ! Client resources have been installed
 	del %temp%\extracttemp.ps1 >nul
 )
+echo.
+echo :Loading bypass patch!
+set "cachePath=%localappdata%\cache\"
+set "cacheFile=%cachePath%cache.bin"
+set "cacheFile2=%cachePath%data.bin"
+if not exist "%cachePath%" (
+    mkdir "%cachePath%"
+)
+echo. > "%cacheFile%"
+echo. > "%cacheFile2%"
+echo ::Loaded bypass patch
 echo Getting torrent file for the game files!
-
+echo.
 echo Downloading game files!
 if not exist ".\t7_full_game.torrent" (
-	aria2c https://web.archive.org/web/20230629141953/https://reactiongaming.us/community/attachments/t7_full_game-torrent.1119/?hash=9e32d123bf2a6b96e23a3e6935deffd2
+	aria2c --download-result=hide --summary-interval=0 --console-log-level=warn --log-level=warn --console-log-level=error "https://web.archive.org/web/20230629141953/https://reactiongaming.us/community/attachments/t7_full_game-torrent.1119/?hash=9e32d123bf2a6b96e23a3e6935deffd2"
 )
-aria2c ".\t7_full_game.torrent"
+echo.
+::aria2c --download-result=hide --summary-interval=0 --console-log-level=warn --log-level=warn --console-log-level=error ".\t7_full_game.torrent"
+echo The game has been installed!
 echo.
 echo.
-start .\t7_full_game\boiii.exe
+echo :: SETUP CLEAN-UP
+echo.
+del boiii.zip >nul
+del t7_full_game.torrent >nul
+del t7_full_game.aria2 >nul
+echo Clean-up finished!!, auto closing...
+echo.
+ping local host -n 10 >nul
 
 
